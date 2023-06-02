fc3601c61072120fd5d0b7cebcbafd4dd9a063f6
Irina <iparakhina91@gmail.com>
2023-06-03 00:47:26 +0300

    updated buildgradle

--- src/test/java/tests/LoginIosTest.java
+++ src/test/java/tests/LoginIosTest.java
@@ -1,7 +1,6 @@
 package tests;
 
 import io.appium.java_client.AppiumBy;
-import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -10,7 +9,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-public class LoginIOSTest extends TestBase {
+public class LoginIosTest extends TestBase {
 
     @Test
     @Tag("ios")
